494239bc732886c79f298d1eec0c41feb148c278
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-27 19:07:30 +0800

    使用Api更新数据

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import Read from './Read'
 import Currently from './Currently'
 import Want from './Want'
@@ -11,53 +11,18 @@ import './App.css'
 
 export default class BooksApp extends Component{
     state = {
-        books:[
-            {title:"bookName1",
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:'currentlyReading',
-                id:'100'
-            },
-            {title:'bookName2',
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:'currentlyReading',
-                id:'102'
-            },
-            {title:'bookName3',
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:'currentlyReading',
-                id:'103'
-            },
-            {title:'bookName4',
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:"wantToRead",
-                id:'104'
-            },
-            {title:'bookName5',
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:"wantToRead",
-                id:'105'
-            },
-            {title:'bookName6',
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:"read",
-                id:'106'
-            },
-            {title:'bookName7',
-                authors:'Harmeet',
-                imageLinks:{smallThumbnail:"http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"},
-                shelf:"read",
-                id:'107'
-            }
-
-        ],
+        books:[],
         value:''
     };
+
+    componentDidMount() {
+        BooksAPI.getAll().then(books=>{
+            this.setState(({
+                books
+            }))
+        })
+
+    }
 
     updateBooks = (bookShelf,bookId)=>{
         let firstShelf = this.state.books.filter((book)=>(book.id ===bookId))
