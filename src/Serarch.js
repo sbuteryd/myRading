46055a7ab01282f0bf9d3129e01b67ae8edc98e1
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-28 11:18:50 +0800

    修复空值报错问题

--- src/Serarch.js
+++ src/Serarch.js
@@ -2,37 +2,28 @@ import React,{Component}from 'react'
 import {Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+// import {update} from "./BooksAPI";
 export default class Serarch extends Component{
     state = {
-        value:'',
         query:'',
         books:[]
     };
-    getDate = ()=>{
-        if(this.state.query){
-            BooksAPI.search(this.state.query).then((book) => {
-                this.setState(({
-                    books:book
-                }))
-            })
-        }
-    }
-    updateSearch = (values)=>{
-        this.setState(()=>({
-            query: values
-        }));
-        if(this.state.query){
-            this.getDate()
-        }
-    }
-    handleChange = (book,event)=>{
-        console.log(event.target)
-        this.props.updateBooks(book,event.target.value)
-
+    updateSearch =(values)=>{
+       this.setState(({
+          query:values
+       }));
+       if(this.state.query){
+           BooksAPI.search(this.state.query).then((books)=>{
+              this.setState(({
+                  books
+              }))
+           })
+       }
     };
 
     render() {
-        {console.log(this.state.books)}
+            console.log(this.state.books)
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -40,44 +31,37 @@ export default class Serarch extends Component{
                         <button className="close-search">Close</button>
                     </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                         <input type="text" value={this.state.query} onChange={(event)=> this.updateSearch(event.target.value)} placeholder="Search by title or author"/>
-
                     </div>
                 </div>
-                <div className="search-books-results">
-                    <ol className="books-grid">
-                        {(this.state.books)&&(
-                            this.state.books.map((book)=>(
-                                    <li key={book.id}>
-                                        <div className='book'>
-                                            <div className='book-top'>
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
-                                                <div className='book-shelf-changer'>
-                                                    <select onChange={(event)=>this.handleChange(book,event)}>
-                                                        <option value="move" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className='book-title'>{book.title}</div>
-                                            <div className='book-authors'>{book.authors}</div>
-                                        </div>
-                                    </li>
-                                ))
-                        )}
 
+                <div className="search-books-results">
+                    <ol className='books-grid'>
+                         {/*设置长度大于1 防止报错*/}
+                    {this.state.books.length >1 &&(
+                        this.state.books.map((book)=>(
+                                <li key={book.id}>
+                                    <div className='book'>
+                                        <div className='book-top'>
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                                            <div className='book-shelf-changer'>
+                                                <select>
+                                                    <option value="move" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className='book-title'>{book.title}</div>
+                                        <div className='book-authors'>{book.authors}</div>
+                                    </div>
+                                </li>
+                        ))
+                    )}
                     </ol>
+
                 </div>
             </div>
         );
