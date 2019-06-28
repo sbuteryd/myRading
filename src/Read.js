0b708a3771c18290ef54d0c5dca56a304df63d0b
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-28 15:10:16 +0800

    Revert "删除不要的组件"
    This reverts commit bcd06aef31b5a192dac3223d2d4df8c87c31bc95.

---
+++ src/Read.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react'
+
+export default class Read extends Component{
+    state = {
+        value:'read'
+    };
+    handleChange = (book,event)=>{
+        this.props.updateBooks(book,event.target.value)
+
+    };
+
+    render() {
+        return (
+           <ol className='books-grid'>
+                {this.props.booksList.map((book)=>(
+                    <li key={book.id}>
+                        <div className='book'>
+                            <div className='book-top'>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                                <div className='book-shelf-changer'>
+                                    <select value={this.state.value} onChange={(event)=>this.handleChange(book,event)}>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className='book-title'>{book.title}</div>
+                            <div className='book-authors'>{book.authors}</div>
+                        </div>
+                    </li>
+                ))}
+
+            </ol>
+        );
+    }
+}
---
+++ src/Want.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react'
+
+export default class Want extends Component{
+    handleChange = (book,event)=>{
+        this.props.updateBooks(book,event.target.value)
+
+    };
+
+    render() {
+        return (
+            <ol className='books-grid'>
+                {this.props.booksList.map((book)=>(
+                    <li key={book.id}>
+                        <div className='book'>
+                            <div className='book-top'>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                                <div className='book-shelf-changer'>
+                                    <select value={book.shelf} onChange={(event)=>this.handleChange(book,event)}>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className='book-title'>{book.title}</div>
+                            <div className='book-authors'>{book.authors}</div>
+                        </div>
+                    </li>
+                ))}
+
+            </ol>
+        );
+    }
+}
