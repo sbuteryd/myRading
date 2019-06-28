324968131485709f69fa53bdbb2aaf131095e66b
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-28 09:49:46 +0800

    设置搜索页面

--- src/Serarch.js
+++ src/Serarch.js
@@ -1,8 +1,32 @@
 import React,{Component}from 'react'
 import {Link} from 'react-router-dom'
-
+import * as BooksAPI from './BooksAPI'
+import './App.css'
 export default class Serarch extends Component{
+    state = {
+        query:'',
+        books:[]
+    };
+    getDate = ()=>{
+        if(this.state.query){
+            BooksAPI.search(this.state.query).then((book) => {
+                this.setState(({
+                    books:book
+                }))
+            })
+        }
+    }
+    updateSearch = (values)=>{
+        this.setState(()=>({
+            query: values
+        }));
+        if(this.state.query){
+            this.getDate()
+        }
+    }
+
     render() {
+        {console.log(this.state.books)}
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -18,12 +42,36 @@ export default class Serarch extends Component{
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input type="text" value={this.state.query} onChange={(event)=> this.updateSearch(event.target.value)} placeholder="Search by title or author"/>
 
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {(this.state.books)&&(
+                            this.state.books.map((book)=>(
+                                    <li key={book.id}>
+                                        <div className='book'>
+                                            <div className='book-top'>
+                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                                                <div className='book-shelf-changer'>
+                                                    <select value={this.state.value} onChange={(event)=>this.handleChange(book,event)}>
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className='book-title'>{book.title}</div>
+                                            <div className='book-authors'>{book.authors}</div>
+                                        </div>
+                                    </li>
+                                ))
+                        )}
+
+                    </ol>
                 </div>
             </div>
         );
